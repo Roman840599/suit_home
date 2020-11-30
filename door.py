980cea837a6f3c0bf421c966f18f677f7f395896
Roman <rtrunin59@gmail.com>
2020-11-30 10:10:18 +0200

    add door(with websocket subscriber)

---
+++ door.py
@@ -0,0 +1,41 @@
+import paho.mqtt.client as mqtt
+import random
+import time
+import os
+
+
+class MyMQTTClass(mqtt.Client):
+    def __init__(self, client_id, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.client_id = client_id
+
+    def res_of_connect(self, client, userdata, flags, rc):
+        print("Result from connect: {}".format(mqtt.connack_string(rc)))
+
+    def res_of_subscribe(self, client, userdata, mid, granted_qos):
+        print("I've subscribed with QoS: {}".format(granted_qos[0]))
+
+    def res_of_message(self, client, userdata, msg):
+        print("Message received. Topic: {}. Payload: {}".format(msg.topic,str(msg.payload)))
+
+    def res_of_publish(self, client, userdata, mid):
+        print(self.client_id, "mid: " + str(mid))
+
+    mqtt.Client.on_connect = res_of_connect
+    mqtt.Client.on_subscribe = res_of_subscribe
+    mqtt.Client.on_message = res_of_message
+    mqtt.Client.on_publish = res_of_publish
+
+    def push_message(self, host_name, topic, QoS):
+        self.connect(host_name)
+        self.loop_start()
+        temperature = random.randrange(0, 2, 1)
+        self.publish(topic, temperature, QoS)
+        self.loop_stop()
+
+pid = os.getpid()
+door = MyMQTTClass(pid)
+
+while True:
+    door.push_message('127.0.0.1', 'door/' + str(pid) + '/status', 2)
+    time.sleep(5)
---
+++ door_subscriber.py
@@ -0,0 +1,36 @@
+import paho.mqtt.client as mqtt
+import asyncio
+import websockets
+from multiprocessing import Process, Queue
+
+
+def recieving_from_mqbroker():
+
+    def on_message(client, userdata, msg):
+        print("Message received. Topic: {}. Payload: {}".format(msg.topic, str(msg.payload)))
+        q.put(str(msg.payload))
+
+    client.on_message = on_message
+    client.loop_start()
+    client.subscribe('door/+/status', 2)
+
+
+def sending_to_django():
+
+    async def sending(websocket, path):
+        while True:
+            m = q.get()
+            await websocket.send(m)
+            await asyncio.sleep(2)
+
+    start_server = websockets.serve(sending, "127.0.0.1", 8000)
+    asyncio.get_event_loop().run_until_complete(start_server)
+    asyncio.get_event_loop().run_forever()
+
+
+if __name__ == '__main__':
+    client = mqtt.Client()
+    client.connect('127.0.0.1')
+    q = Queue()
+    Process(target=recieving_from_mqbroker()).start()
+    Process(target=sending_to_django).start()
